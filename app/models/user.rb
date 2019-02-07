5ece58cc14b127efc4465d000b46896bf97d461d
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-02-07 16:13:01 -0200

    Created Users Model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, length: { minimum: 6 }
+end
