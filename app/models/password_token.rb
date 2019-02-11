61bf41b7c6b75c082f946a1e92dd267265ae9ee1
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-02-11 16:16:01 -0200

    Created Tests

--- app/models/password_token.rb
+++ app/models/password_token.rb
@@ -1,3 +1,10 @@
 class PasswordToken < ApplicationRecord
+  has_secure_token
+
   belongs_to :user
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, if: :is_verify_email_token?
+
+  def is_verify_email_token?
+    verify_email
+  end
 end
