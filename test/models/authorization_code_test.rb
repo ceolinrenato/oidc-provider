518faeaa978a7782540f1973273abf8276b70d1e
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-02-13 14:27:12 -0200

    Model and Tests

---
+++ test/models/authorization_code_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class AuthorizationCodeTest < ActiveSupport::TestCase
+
+  def dummy_authorization_code
+    {
+      user: users(:example),
+      relying_party: relying_parties(:example),
+      redirect_uri: redirect_uris(:example),
+      used: false
+    }
+  end
+
+  test "should_create_valid_authorization_code" do
+    authorization_code = AuthorizationCode.new dummy_authorization_code
+    assert authorization_code.save
+  end
+
+end
