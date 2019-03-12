86e4fcdd7aa8ae76e8b5f52e6ad3634db1be8452
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-03-12 15:18:20 -0300

    Adaptado para usar cookies no device_token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   include DeviceHelper
 
   def index_by_device
-    set_device
+    set_device!
     render json: SessionCollectionSerializer.new(@device.sessions)
   rescue CustomExceptions::InvalidRequest => exception
     render json: ErrorSerializer.new(exception), status: :bad_request
--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module OIDCProvider
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
     config.autoload_paths += %W(#{config.root}/lib/modules)
     config.time_zone = 'Brasilia'
   end
