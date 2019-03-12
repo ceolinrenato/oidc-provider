c8da12d16ab4b68af5352b6dad891063ce968645
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-03-12 15:37:25 -0300

    Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost'
+    origins 'http://localhost:4200'
 
     resource '*',
       headers: :any,
