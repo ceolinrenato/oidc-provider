260710070e093bfff9d79ad9d288cc124bc4472b
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-03-14 10:13:26 -0300

    return frontchannel_logout_uris in sessions collection

--- app/serializers/session_collection_serializer.rb
+++ app/serializers/session_collection_serializer.rb
@@ -2,10 +2,14 @@ class SessionCollectionSerializer < BaseCollectionSerializer
 
   def initialize(sessions)
     collection = sessions.map do |session|
+      logout_uris = session.access_tokens.map do |access_token|
+        access_token.relying_party.frontchannel_logout_uri
+      end
       {
         full_name: session.user.full_name,
         email: session.user.email,
-        last_activity: session.last_activity
+        last_activity: session.last_activity,
+        frontchannel_logout_uris: logout_uris.uniq
       }
     end
     super(collection)
