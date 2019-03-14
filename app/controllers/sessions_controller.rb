650521569a91aed0bd5425d007a99c692dedcca3
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-03-14 11:56:37 -0300

    Adicionando transaction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,11 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    set_device!
-    set_session_by_token! raise_on_expired: false, raise_on_signed_out: false
-    @session.destroy
+    ActiveRecord::Base.transaction do
+      set_device!
+      set_session_by_token! raise_on_expired: false, raise_on_signed_out: false
+      @session.destroy!
+    end
     head :no_content
   rescue CustomExceptions::InvalidRequest => exception
     render json: ErrorSerializer.new(exception), status: :bad_request
