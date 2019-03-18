b2a9490ab840afab6257ee62dd2d26538dc0c249
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-03-18 18:56:21 -0300

    Test for Device#active_session_count

--- app/models/device.rb
+++ app/models/device.rb
@@ -3,8 +3,7 @@ class Device < ApplicationRecord
   has_many :sessions
 
   def active_session_count
-    active_sessions = sessions.map { |session| session.active? }.select { |active| active }
-    active_sessions.count
+    sessions.where('signed_out = :signed_out AND last_activity > :last_activity', { signed_out: false, last_activity: Time.now - Session::SESSION_EXPIRATION_TIME }).count
   end
 
 end
