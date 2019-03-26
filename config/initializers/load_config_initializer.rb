672c8068f24c54f199ed8f3a8f2a1ddedf32c772
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-03-26 11:41:03 -0300

    Condigs and Keys

--- config/initializers/load_config_initializer.rb
+++ config/initializers/load_config_initializer.rb
@@ -1,2 +1,4 @@
 sign_in_config = File.read Rails.root.join "config/sign_in_service.yml"
+oidc_provider_config = File.read Rails.root.join "config/oidc_provider.yml"
 SIGN_IN_SERVICE_CONFIG = YAML.load(sign_in_config)[Rails.env]
+OIDC_PROVIDER_CONFIG = YAML.load(oidc_provider_config)[Rails.env]
