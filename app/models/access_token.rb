f22ad89250d74d71c9b321d73a1d27715d4729de
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-03-27 10:31:56 -0300

    AccessToken Generation

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -8,4 +8,31 @@ class AccessToken < ApplicationRecord
   has_many :access_token_scopes, dependent: :destroy
   has_many :scopes, through: :access_token_scopes
 
+  def token
+    payload = {
+      iss: OIDC_PROVIDER_CONFIG[:iss],
+      sub: session.user.id.to_s,
+      aud: relying_party.client_id,
+      exp: created_at.to_i + OIDC_PROVIDER_CONFIG[:expiration_time],
+      iat: created_at.to_i,
+      sid: session.token,
+      scopes: scopes.map { |scope| scope.name }
+    }
+    encrypt(jwt_encode(payload))
+  end
+
+  private
+
+  def encrypt(data)
+    cipher = OpenSSL::Cipher::AES256.new(:CBC)
+    cipher.encrypt
+    cipher.key = TokenDecode::AES_KEY
+    cipher.iv = TokenDecode::AES_IV
+    Base64.encode64(cipher.update(data) + cipher.final)
+  end
+
+  def jwt_encode(payload)
+    JWT.encode payload, TokenDecode::RSA_PRIVATE, 'RS256'
+  end
+
 end
--- test/models/access_token_test.rb
+++ test/models/access_token_test.rb
@@ -15,4 +15,16 @@ class AccessTokenTest < ActiveSupport::TestCase
     assert access_token.save
   end
 
+  test "token_method_must_generate_valid_encrypted_jws_token" do
+    token = access_tokens(:example).token
+    decoded_token = TokenDecode::AccessToken.new(token).decode
+    payload = decoded_token.first
+    headers = decoded_token.last
+    assert_equal payload["sub"], access_tokens(:example).session.user.id.to_s
+    assert_equal payload["aud"], access_tokens(:example).relying_party.client_id
+    assert_equal payload["sid"], access_tokens(:example).session.token
+    assert_equal payload["scopes"], access_tokens(:example).scopes.map { |scope| scope.name }
+    assert_equal headers["alg"], 'RS256'
+  end
+
 end
