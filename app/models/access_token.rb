1bcd390fa7fad6da974d5316cbe93955d28f47a7
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-04-03 17:53:33 -0300

    Include kid

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -53,7 +53,7 @@ class AccessToken < ApplicationRecord
   end
 
   def jwt_encode(payload)
-    JWT.encode payload, TokenDecode::RSA_PRIVATE, 'RS256'
+    JWT.encode payload, TokenDecode::RSA_PRIVATE, 'RS256', { kid: 'Key used to validate id_token signature'}
   end
 
   def calc_at_hash(encrypted_access_token)
