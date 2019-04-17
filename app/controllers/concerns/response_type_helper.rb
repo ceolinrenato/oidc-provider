dd1ac9586eba7a2d42f1f022fe9073677b29bf1a
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-04-17 14:00:19 -0300

    Adiciado todos os flows para Third Party applications

--- app/controllers/concerns/response_type_helper.rb
+++ app/controllers/concerns/response_type_helper.rb
@@ -13,7 +13,19 @@ module ResponseTypeHelper
     'code token'
   ]
 
-  AUTHORIZED_RESPONSE_TYPES = ['code']
+  if Rails.env.test?
+    AUTHORIZED_RESPONSE_TYPES = ['code']
+  else
+    AUTHORIZED_RESPONSE_TYPES = [
+      'code',
+      'id_token',
+      'id_token token',
+      'token',
+      'code id_token',
+      'code id_token token',
+      'code token'
+    ]
+  end
 
   def set_response_type
     raise CustomExceptions::InvalidRequest.new 12 unless params[:response_type]
