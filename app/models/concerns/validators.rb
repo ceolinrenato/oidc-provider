4790992c08866f7de1c5824861ca6c795efd3b96
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-04-22 13:29:26 -0300

    Corrigindo code smell apontado por SonarQube

--- app/models/concerns/validators.rb
+++ app/models/concerns/validators.rb
@@ -3,16 +3,12 @@ module Validators
     def validate_each(record, attribute, value)
       if options[:https]
         uri = URI(value)
-        unless uri.scheme == 'https'
-          unless options[:https][:allow_on_localhost] && uri.hostname == 'localhost'
-            record.errors[attribute] << (options[:message] || 'uri must use https scheme')
-          end
+        unless uri.scheme == 'https' || (options[:https][:allow_on_localhost] && uri.hostname == 'localhost')
+          record.errors[attribute] << (options[:message] || 'uri must use https scheme')
         end
       end
-      if options[:deny_localhost]
-        unless URI(value).hostname != 'localhost'
-          record.errors[attribute] << (options[:message] || 'use of localhost as hostname is denied')
-        end
+      if options[:deny_localhost] && URI(value).hostname == 'localhost'
+        record.errors[attribute] << (options[:message] || 'use of localhost as hostname is denied')
       end
     end
   end
