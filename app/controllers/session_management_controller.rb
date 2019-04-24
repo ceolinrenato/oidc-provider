a8b1305d28ff16ed4ee5df61fc718a4dd9f59695
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-04-24 16:38:34 -0300

    Removing individual user sessions on devices

--- app/controllers/session_management_controller.rb
+++ app/controllers/session_management_controller.rb
@@ -2,6 +2,10 @@ class SessionManagementController < ApplicationController
 
   include SessionHelper
   include DeviceHelper
+  include UserHelper
+  include RelyingPartyHelper
+
+  before_action :bearer_authorization, only: [:destroy_user_session]
 
   def index_by_device
     set_device!
@@ -17,12 +21,12 @@ class SessionManagementController < ApplicationController
   def destroy
     ActiveRecord::Base.transaction do
       set_device!
-      set_session_by_token! raise_on_expired: false, raise_on_signed_out: false, raise_on_aged: false
+      set_device_session_by_token!
       @session.destroy!
     end
     head :no_content
-  rescue CustomExceptions::InvalidRequest => exception
-    render json: ErrorSerializer.new(exception), status: :bad_request
+  rescue CustomExceptions::EntityNotFound => exception
+    render json: ErrorSerializer.new(exception), status: :not_found
   rescue CustomExceptions::UnrecognizedDevice => exception
     clear_device_token_cookie
     render json: ErrorSerializer.new(exception), status: :bad_request
@@ -31,15 +35,38 @@ class SessionManagementController < ApplicationController
     render json: ErrorSerializer.new(exception), status: :bad_request
   end
 
+  def destroy_user_session
+    ActiveRecord::Base.transaction do
+      set_user_by_id!
+      target_user_authorization
+      third_party_authorization
+      set_user_session_by_token!
+      set_device!
+      raise CustomExceptions::InsufficientPermissions.new 40 if @device == @session.device
+      @session.destroy!
+    end
+    head :no_content
+  rescue CustomExceptions::EntityNotFound => exception
+    render json: ErrorSerializer.new(exception), status: :not_found
+  rescue CustomExceptions::UnrecognizedDevice => exception
+    clear_device_token_cookie
+    render json: ErrorSerializer.new(exception), status: :bad_request
+  rescue CustomExceptions::CompromisedDevice => exception
+    destroy_compromised_device
+    render json: ErrorSerializer.new(exception), status: :bad_request
+  rescue CustomExceptions::InsufficientPermissions => exception
+    render json: ErrorSerializer.new(exception), status: :forbidden
+  end
+
   def sign_out
     ActiveRecord::Base.transaction do
       set_device!
-      set_session_by_token! raise_on_expired: false, raise_on_signed_out: false, raise_on_aged: false
+      set_device_session_by_token!
       @session.update! signed_out: true
     end
     render json: SessionSerializer.new(@session)
-  rescue CustomExceptions::InvalidRequest => exception
-    render json: ErrorSerializer.new(exception), status: :bad_request
+  rescue CustomExceptions::EntityNotFound => exception
+    render json: ErrorSerializer.new(exception), status: :not_found
   rescue CustomExceptions::UnrecognizedDevice => exception
     clear_device_token_cookie
     render json: ErrorSerializer.new(exception), status: :bad_request
