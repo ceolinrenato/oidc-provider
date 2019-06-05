0ec4a7b4b33215e309c8ade066f0ff2f976d754e
Renato Ceolin <renato.ceolin@renatoceolin.com>
2019-06-05 13:50:23 -0300

    Fixed TrailingCommaInHashLiteral

--- test/models/access_token_test.rb
+++ test/models/access_token_test.rb
@@ -5,7 +5,7 @@ class AccessTokenTest < ActiveSupport::TestCase
     {
       authorization_code: authorization_codes(:example),
       relying_party: relying_parties(:example),
-      session: sessions(:example),
+      session: sessions(:example)
     }
   end
 
